d597f8f20dcb2c77169b9da5cbb1948a9d432de2
Larissa Nagiller <larissa.nagiller@gmail.com>
2025-10-30 16:33:11 +0100

    AboutUs Page + Tiernotruf Page + CSS Sytles
    hinzugefügt und zum Teil befüllt

---
+++ Homepage/AboutUs.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php
+$PageTitle = "AboutUs";
+include_once('header.php');
+?>
+<style>
+p {
+    background: linear-gradient(black, red);
+    -webkit-background-clip: text;
+            background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-family:arial
+    
+}
+</style>
+
+<div class ="text-div">
+    <p class ="info-text"> Wir sind das Pissy Tierheim!</br> </br>
+  Das Pissy Tierheim gibt es seit September 2025 und ist ein rein fiktionaler Ort in den Köpfen von Helm und Laris. </br></br>
+  Solltest du doch auf dieser Seite gelandet sein, kommst du nie wieder hier weg. </br></br>
+  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...
+  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...
+  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...</br>
+  Komm her wenn du dich traust, und nimm ein Tier mit nach Hause!</p>
+
+  
+</div>
+
+
+
+
---
+++ Homepage/TierNotruf.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+$PageTitle = "TierNotruf";
+include_once('header.php');
+?>
+<link rel="stylesheet" href="style.css">
+
+<div class="info-text">
+        
+    <h2>Braucht ein Tier Hilfe?</h2></br> </br>
+    <p class ="info-text"> Bei Tips zu ausgesetzten Tieren rufe hier an:</br> </br>
+        1234567890</br> </br>
+        Bei verletzten Wildtieren rufe hier an:</br> </br>
+        1234567890</br> </br>
+        Alle anderen Anfragen:</br> </br>
+        1234567890
+
+  </p>
+
+
+
+
+<p><img src="Tierarzt.jpeg" alt="Homepage Foto" style="width:250px; height:170px;" class="center"></p>
+</div>
