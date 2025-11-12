6d2cd0a618ca136405c119f23b0e2350f4fcaa2d
Larissa <larissa.nagiller@gmail.com>
2025-11-12 17:59:39 +0100

    Korrektur GIT 3rd Device
    AboutUs PHP

--- Homepage/AboutUs.php
+++ Homepage/AboutUs.php
@@ -3,7 +3,9 @@
 $PageTitle = "AboutUs";
 include_once('header.php');
 ?>
-<style>
+<link rel="stylesheet" href="style.css">
+
+<!-- <style>
 p {
     background: linear-gradient(black, red);
     -webkit-background-clip: text;
@@ -12,19 +14,43 @@ p {
     font-family:arial
     
 }
-</style>
+</style> -->
+<div class="title">
+    <h1> Seit 2025 </h1>
+</div>
 
-<div class ="text-div">
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class ="text-div">
+        <p class ="info-text"> Wir sind das Pissy Tierheim!</br> </br>
+        Das Pissy Tierheim gibt es seit September 2025 und ist ein rein fiktionaler Ort in den Köpfen von Helm und Laris. </br></br>
+        Seit Jahren beschäftigen wir uns aktiv mit dem Tierschutz; sei es durch das Füttern von Stadttauben, das Adoptieren von geretteten Laborratten oder den Erwerb von Patenschaften für bedrohte Tierarten.<br></br>
+        Das Pissy Tierheim liegt seit der Erstehung im 15. Bezirk, dem Herzstück von Wien.</p>
+      </div>
+    </div>
+  <div class="col">
+    <p><img src="Rattis.jpeg" alt="Ratten im Tierheim Foto" style="width:250px; height:170px;" class="center"></img></p>
+  </div>
+</div>
+</div>
+  
+
+<!-- <div class ="text-div">
     <p class ="info-text"> Wir sind das Pissy Tierheim!</br> </br>
   Das Pissy Tierheim gibt es seit September 2025 und ist ein rein fiktionaler Ort in den Köpfen von Helm und Laris. </br></br>
-  Solltest du doch auf dieser Seite gelandet sein, kommst du nie wieder hier weg. </br></br>
-  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...
-  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...
-  Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...Nichts hier ist Realität...</br>
-  Komm her wenn du dich traust, und nimm ein Tier mit nach Hause!</p>
+  Seit Jahren beschäftigen wir uns aktiv mit dem Tierschutz; sei es durch das Füttern von Stadttauben, das Adoptieren von geretteten Laborratten oder den Erwerb von Patenschaften für bedrohte Tierarten.<br></br>
+  Das Pissy Tierheim liegt seit der Erstehung im 15. Bezirk, dem Herzstück von Wien.</p>
 
   
-</div>
+</div> -->
+
+
+
+
+
 
 
 
