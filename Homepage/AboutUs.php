a7f0d090c01b58a58c7aac928d1b498a041b6282
Larissa Nagiller <larissa.nagiller@gmail.com>
2025-11-12 18:55:55 +0100

    Bootstrap int
    AboutUs

--- Homepage/AboutUs.php
+++ Homepage/AboutUs.php
@@ -3,6 +3,11 @@
 $PageTitle = "AboutUs";
 include_once('header.php');
 ?>
+<link 
+  rel="stylesheet" 
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+  integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+  crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 
 <!-- <style>
@@ -23,18 +28,19 @@ p {
 
 <div class="container">
   <div class="row">
-    <div class="col">
+    <div class="col-md-4">
       <div class ="text-div">
         <p class ="info-text"> Wir sind das Pissy Tierheim!</br> </br>
         Das Pissy Tierheim gibt es seit September 2025 und ist ein rein fiktionaler Ort in den Köpfen von Helm und Laris. </br></br>
-        Seit Jahren beschäftigen wir uns aktiv mit dem Tierschutz; sei es durch das Füttern von Stadttauben, das Adoptieren von geretteten Laborratten oder den Erwerb von Patenschaften für bedrohte Tierarten.<br></br>
+        Seit Jahren beschäftigen wir uns aktiv mit dem Tierschutz; sei es durch die Betreuung der 3 größten Taubenschläge in Wien, das Adoptieren von geretteten Laborratten oder den Erwerb von Patenschaften für bedrohte Tierarten.<br></br>
         Das Pissy Tierheim liegt seit der Erstehung im 15. Bezirk, dem Herzstück von Wien.</p>
       </div>
     </div>
-  <div class="col">
-    <p><img src="Rattis.jpeg" alt="Ratten im Tierheim Foto" style="width:250px; height:170px;" class="center"></img></p>
+    <div class="col-md-auto">
+      <div class="image">
+        <p><img src="Rattis.jpeg" alt="Ratten im Tierheim Foto" style="width:100px; height:50px;" class="center"></img></p>
+    </div>
   </div>
-</div>
 </div>
   
 
