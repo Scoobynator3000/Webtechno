ca211c7b7c900d6fe7301f211310b7790e68d19e
Larissa Nagiller <larissa.nagiller@gmail.com>
2025-11-16 00:01:10 +0100

    MitarbeiterView

---
+++ Homepage/MitarbeiterView.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<?php $PageTitle = "Larissa"; 
+include_once('header.php'); 
+?> 
+<div class="container">
+<div class="text-div">
+        <p class="info-text">
+            Derzeit sind keine Termine verbucht.
+        </p>
+
+</div>
+</div>
+      
+      
