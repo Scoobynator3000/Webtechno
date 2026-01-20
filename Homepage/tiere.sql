eaefa90e5f81d6a1dcaeffe2c3eed3aed37afda0
Larissa Nagiller <larissa.nagiller@gmail.com>
2026-01-20 12:39:44 +0100

    neue db

--- Homepage/tiere.sql
+++ Homepage/tiere.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 19. Jan 2026 um 09:37
+-- Erstellungszeit: 20. Jan 2026 um 12:39
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `hunde` (
   `Name` text NOT NULL,
   `Age` int(11) NOT NULL,
   `Vorerfahrung` tinyint(1) NOT NULL,
-  `vermittelbar` tinyint(1) NOT NULL,
+  `vermittelbar` varchar(255) NOT NULL,
   `Beschreibung` text NOT NULL,
   `Foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,10 +42,10 @@ CREATE TABLE `hunde` (
 --
 
 INSERT INTO `hunde` (`ID`, `Name`, `Age`, `Vorerfahrung`, `vermittelbar`, `Beschreibung`, `Foto`) VALUES
-(1, 'Mausi', 3, 1, 0, 'Mausi ist grundsätzlich ein offener Hund, der freundlich auf Menschen zugeht. Bei manchen Menschen ist er anfangs aber eher etwas skeptischer und es kann vorkommen, dass er diese verbellt. In der Regel ist das Eis aber schnell gebrochen und Mausi zeigt dann seine aufgeweckte und lustige Art.', 'adoptiere\\hunde\\Mausi.jpg'),
-(2, 'Schnucki', 3, 1, 1, 'Schnucki ist grundsätzlich eine freundliche Hündin, möchte von Fremden aber nicht gleich mit Streicheleinheiten überfallen werden. Beim Kennenlernen von Fremden erwartet sie sich eine gewisse Individualdistanz von ihrem Gegenüber, um sich in ihrem Tempo annähern zu können. Bedrängt man sie nicht und hat vielleicht auch noch das ein oder andere Leckerli in der Tasche, ist das Eis schnell gebrochen.', 'adoptiere\\hunde\\Schnucki.jpg'),
-(3, 'Baby', 12, 1, 1, 'Baby liebt Leckerlis in jeder Form und Geschmacksrichtung, zu jeder Tages-, und Nachtzeit und in jeder Situation. Mavis Vorliebe für Leckerlis ist allerdings Fluch und Segen zugleich. Im Training ist sie top motiviert, liebt Suchspiele jeder Art und würde alles für Futter tun. Gleichzeitig kann Baby allerdings sehr fordernd werden und man muss sie hin und wieder daran erinnern, dass man sich nicht selbst an Taschen oder Leckerlibeuteln bedient oder durch frustiges Bellen nicht unbedingt mehr Leckerlis bekommt.', 'adoptiere\\hunde\\Baby.jpg'),
-(4, 'Ilya', 16, 0, 1, 'Bei Hundebegegnungen in moderater Distanz ist Ilya bereits gut ansprechbar. Ist der andere Hund zu Nahe, kann es vorkommen das sie in die Leine springt und bellt. Im direkten Kontakt entscheidet die Sympathie. Im neuen Zuhause möchte Ilya definitiv Einzelprinzessin sein, weshalb keine Artgenossen oder andere Tiere im gemeinsamen Haushalt leben sollten. Ilya kann problemlos einen Maulkorb tragen, fährt brav im Auto mit und kennt die Grundkommandos.', 'adoptiere\\hunde\\Ilya.jpg');
+(1, 'Mausi', 3, 1, 'Nein - Patenschaft möglich', 'Mausi ist grundsätzlich ein offener Hund, der freundlich auf Menschen zugeht. Bei manchen Menschen ist er anfangs aber eher etwas skeptischer und es kann vorkommen, dass er diese verbellt. In der Regel ist das Eis aber schnell gebrochen und Mausi zeigt dann seine aufgeweckte und lustige Art.', 'adoptiere\\hunde\\Mausi.jpg'),
+(2, 'Schnucki', 3, 1, 'Ja', 'Schnucki ist grundsätzlich eine freundliche Hündin, möchte von Fremden aber nicht gleich mit Streicheleinheiten überfallen werden. Beim Kennenlernen von Fremden erwartet sie sich eine gewisse Individualdistanz von ihrem Gegenüber, um sich in ihrem Tempo annähern zu können. Bedrängt man sie nicht und hat vielleicht auch noch das ein oder andere Leckerli in der Tasche, ist das Eis schnell gebrochen.', 'adoptiere\\hunde\\Schnucki.jpg'),
+(3, 'Baby', 12, 1, 'Ja', 'Baby liebt Leckerlis in jeder Form und Geschmacksrichtung, zu jeder Tages-, und Nachtzeit und in jeder Situation. Mavis Vorliebe für Leckerlis ist allerdings Fluch und Segen zugleich. Im Training ist sie top motiviert, liebt Suchspiele jeder Art und würde alles für Futter tun. Gleichzeitig kann Baby allerdings sehr fordernd werden und man muss sie hin und wieder daran erinnern, dass man sich nicht selbst an Taschen oder Leckerlibeuteln bedient oder durch frustiges Bellen nicht unbedingt mehr Leckerlis bekommt.', 'adoptiere\\hunde\\Baby.jpg'),
+(4, 'Ilya', 16, 0, 'Ja', 'Bei Hundebegegnungen in moderater Distanz ist Ilya bereits gut ansprechbar. Ist der andere Hund zu Nahe, kann es vorkommen das sie in die Leine springt und bellt. Im direkten Kontakt entscheidet die Sympathie. Im neuen Zuhause möchte Ilya definitiv Einzelprinzessin sein, weshalb keine Artgenossen oder andere Tiere im gemeinsamen Haushalt leben sollten. Ilya kann problemlos einen Maulkorb tragen, fährt brav im Auto mit und kennt die Grundkommandos.', 'adoptiere\\hunde\\Ilya.jpg');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,7 @@ CREATE TABLE `katzen` (
   `Name` text NOT NULL,
   `Age` int(11) NOT NULL,
   `Freiganger` tinyint(1) NOT NULL,
-  `vermittelbar` tinyint(1) NOT NULL,
+  `vermittelbar` varchar(255) NOT NULL,
   `Beschreibung` text NOT NULL,
   `Foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -88,9 +88,9 @@ CREATE TABLE `katzen` (
 --
 
 INSERT INTO `katzen` (`ID`, `Name`, `Age`, `Freiganger`, `vermittelbar`, `Beschreibung`, `Foto`) VALUES
-(1, 'Riso & Helmi', 24, 0, 0, 'Riso und Helmi sind zwei sehr unterschiedliche Katzen, die sich allerdings wunderbar ergänzen und nur gemeinsam in ein neues Zuhause ziehen möchten.\r\n\r\nHelmi ist ein entzückender rot-weißer Wirbelwind, der alles mit vollem Körpereinsatz angeht. Ob Spielen, Erkunden oder Kuscheln – Helmi ist immer zu 100 % dabei. Sie ist sehr liebesbedürftig, sucht intensiv den Kontakt zu seinen Menschen und braucht viel Beschäftigung und Action. Still sitzen ist nichts für sie, denn ihre Neugier und ihr Bewegungsdrang wollen täglich ausgelebt werden.\r\n\r\nRiso ist der gemütlichere Teil dieses Duos. Der hübsche Britisch-Mix liebt es zu plaudern, zu kuscheln und die Nähe ihrer Menschen zu genießen. Sie spielt und rauft gerne mit Helmi, weiß aber auch, wann genug ist, und zieht sich dann lieber zum Entspannen zurück.', 'adoptiere\\katzen\\Riso_Helmi.jpg'),
-(2, 'Fabi', 25, 1, 0, 'Fabi ist ein freundlicher, aufgeweckter Kater mit großem Entdeckergeist. Er ist sehr neugierig, immer unterwegs und möchte bei allem dabei sein. Dabei hat er allerdings auch einen ausgeprägten eigenen Kopf – die Dinge sollten idealerweise nach seinen Vorstellungen laufen. Wird ihm etwas zu viel oder läuft nicht in seinem Sinne, kann er durchaus unwirsch reagieren und das klar kommunizieren.\r\n\r\n', 'adoptiere\\katzen\\Fabi.jpg'),
-(3, 'Bärbel', 2, 1, 1, 'Bärbel ist eine neugierige und aufmerksame Katzendame, die viel Interesse an ihrer Umgebung zeigt. Im Zimmer sitzt sie oft bei der Tür und beobachtet genau, was draußen passiert – sie wäre also in einem Zuhause mit mehr Bewegungsfreiheit bestimmt glücklich.\r\n\r\nKommt man zu ihr, streift sie einem manchmal freundlich um die Beine und sucht vorsichtige Nähe. Berührungen lässt sie punktuell zu, doch schnell wird ihr körperliche Nähe zu viel. Dann zeigt sie deutlich, dass sie Abstand möchte und kann auch wehrhaft reagieren. Hochheben oder ausgiebiges Kuscheln gehören derzeit nicht zu ihrem Repertoire.', 'adoptiere\\katzen\\Bärbel.jpg');
+(1, 'Riso & Helmi', 24, 0, 'Nein - Patenschaft möglich', 'Riso und Helmi sind zwei sehr unterschiedliche Katzen, die sich allerdings wunderbar ergänzen und nur gemeinsam in ein neues Zuhause ziehen möchten.\r\n\r\nHelmi ist ein entzückender rot-weißer Wirbelwind, der alles mit vollem Körpereinsatz angeht. Ob Spielen, Erkunden oder Kuscheln – Helmi ist immer zu 100 % dabei. Sie ist sehr liebesbedürftig, sucht intensiv den Kontakt zu seinen Menschen und braucht viel Beschäftigung und Action. Still sitzen ist nichts für sie, denn ihre Neugier und ihr Bewegungsdrang wollen täglich ausgelebt werden.\r\n\r\nRiso ist der gemütlichere Teil dieses Duos. Der hübsche Britisch-Mix liebt es zu plaudern, zu kuscheln und die Nähe ihrer Menschen zu genießen. Sie spielt und rauft gerne mit Helmi, weiß aber auch, wann genug ist, und zieht sich dann lieber zum Entspannen zurück.', 'adoptiere\\katzen\\Riso_Helmi.jpg'),
+(2, 'Fabi', 25, 1, 'Nein - Patenschaft möglich', 'Fabi ist ein freundlicher, aufgeweckter Kater mit großem Entdeckergeist. Er ist sehr neugierig, immer unterwegs und möchte bei allem dabei sein. Dabei hat er allerdings auch einen ausgeprägten eigenen Kopf – die Dinge sollten idealerweise nach seinen Vorstellungen laufen. Wird ihm etwas zu viel oder läuft nicht in seinem Sinne, kann er durchaus unwirsch reagieren und das klar kommunizieren.\r\n\r\n', 'adoptiere\\katzen\\Fabi.jpg'),
+(3, 'Bärbel', 2, 1, 'Ja', 'Bärbel ist eine neugierige und aufmerksame Katzendame, die viel Interesse an ihrer Umgebung zeigt. Im Zimmer sitzt sie oft bei der Tür und beobachtet genau, was draußen passiert – sie wäre also in einem Zuhause mit mehr Bewegungsfreiheit bestimmt glücklich.\r\n\r\nKommt man zu ihr, streift sie einem manchmal freundlich um die Beine und sucht vorsichtige Nähe. Berührungen lässt sie punktuell zu, doch schnell wird ihr körperliche Nähe zu viel. Dann zeigt sie deutlich, dass sie Abstand möchte und kann auch wehrhaft reagieren. Hochheben oder ausgiebiges Kuscheln gehören derzeit nicht zu ihrem Repertoire.', 'adoptiere\\katzen\\Bärbel.jpg');
 
 -- --------------------------------------------------------
 
@@ -102,7 +102,7 @@ CREATE TABLE `kleintiere` (
   `id` int(11) NOT NULL,
   `Name` text NOT NULL,
   `Art` text NOT NULL,
-  `Vermittelbar` tinyint(1) NOT NULL,
+  `Vermittelbar` varchar(255) NOT NULL,
   `Beschreibung` text NOT NULL,
   `Foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -112,9 +112,9 @@ CREATE TABLE `kleintiere` (
 --
 
 INSERT INTO `kleintiere` (`id`, `Name`, `Art`, `Vermittelbar`, `Beschreibung`, `Foto`) VALUES
-(1, 'Pesto', 'Ratte', 0, 'Aufgrund eines zu frechen Charakters leider nicht vermittelbar.', 'adoptiere\\kleintiere\\Pesto.jpg'),
-(2, 'Schnuppi', 'Hamster', 1, 'Die kleine Hamsterdame Schnuppi sucht nach einem schönen neuen Zuhause mit einem geräumigen und spannend eingericheten Gehege! ', 'adoptiere\\kleintiere\\Schnuppi.jpg'),
-(3, 'Konrad', 'Wühlmaus', 0, 'Konrad wurde verletzt aufgefunden und wird von unserem Team gepflegt, steht aber nicht zur Vermittlung frei.', 'adoptiere\\kleintiere\\Konrad.jpg');
+(1, 'Pesto', 'Ratte', 'Nein - Patenschaft möglich', 'Aufgrund eines zu frechen Charakters leider nicht vermittelbar.', 'adoptiere\\kleintiere\\Pesto.jpg'),
+(2, 'Schnuppi', 'Hamster', 'Ja', 'Die kleine Hamsterdame Schnuppi sucht nach einem schönen neuen Zuhause mit einem geräumigen und spannend eingericheten Gehege! ', 'adoptiere\\kleintiere\\Schnuppi.jpg'),
+(3, 'Konrad', 'Wühlmaus', 'Nein - Patenschaft möglich', 'Konrad wurde verletzt aufgefunden und wird von unserem Team gepflegt, steht aber nicht zur Vermittlung frei.', 'adoptiere\\kleintiere\\Konrad.jpg');
 
 -- --------------------------------------------------------
 
@@ -146,7 +146,7 @@ CREATE TABLE `vögel` (
   `id` int(11) NOT NULL,
   `Name` text NOT NULL,
   `Art` text NOT NULL,
-  `Vermittelbar` tinyint(1) NOT NULL,
+  `Vermittelbar` varchar(255) NOT NULL,
   `Beschreibung` text NOT NULL,
   `Foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -156,8 +156,8 @@ CREATE TABLE `vögel` (
 --
 
 INSERT INTO `vögel` (`id`, `Name`, `Art`, `Vermittelbar`, `Beschreibung`, `Foto`) VALUES
-(1, 'Barbara', 'Wellensittich', 1, 'Barbara braucht viel Vorerfahrung und einen sicheren Besitzer, der sie auch medizinisch versorgen kann.', 'adoptiere\\vogel\\Babs.jpg'),
-(2, 'Diana', 'Taube', 0, 'Diana kann zurzeit leider nicht vermittelt werden.', 'adoptiere\\vogel\\Taube.jpg');
+(1, 'Barbara', 'Wellensittich', 'Ja', 'Barbara braucht viel Vorerfahrung und einen sicheren Besitzer, der sie auch medizinisch versorgen kann.', 'adoptiere\\vogel\\Babs.jpg'),
+(2, 'Diana', 'Taube', 'Nein - Patenschaft möglich', 'Diana kann zurzeit leider nicht vermittelt werden.', 'adoptiere\\vogel\\Taube.jpg');
 
 --
 -- Indizes der exportierten Tabellen
@@ -241,7 +241,7 @@ ALTER TABLE `mauser`
 -- AUTO_INCREMENT für Tabelle `vögel`
 --
 ALTER TABLE `vögel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
